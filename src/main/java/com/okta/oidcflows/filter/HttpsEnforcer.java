6b10049d3e97ba7006a720a90c855c8530932f44
Micah Silverman <micah@afitnerd.com>
2017-07-12 23:21:49 -0700

    Setup https redirect

--- src/main/java/com/okta/oidcflows/filter/HttpsEnforcer.java
+++ src/main/java/com/okta/oidcflows/filter/HttpsEnforcer.java
@@ -29,7 +29,7 @@ public class HttpsEnforcer implements Filter {
 
         if (request.getHeader(X_FORWARDED_PROTO) != null) {
             if (request.getHeader(X_FORWARDED_PROTO).indexOf("https") != 0) {
-                response.sendRedirect("https://" + request.getServerName() + request.getPathInfo());
+                response.sendRedirect("https://" + request.getServerName() + ((request.getPathInfo() == null) ? "" : request.getPathInfo()));
                 return;
             }
         }
