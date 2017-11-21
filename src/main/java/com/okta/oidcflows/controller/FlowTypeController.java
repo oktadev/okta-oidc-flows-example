db28ec75532235805804867218f619ae58304e76
Micah Silverman <micah@afitnerd.com>
2017-11-21 16:28:26 -0500

    Updated to only show game link if okta.zork.url is configured.

--- src/main/java/com/okta/oidcflows/controller/FlowTypeController.java
+++ src/main/java/com/okta/oidcflows/controller/FlowTypeController.java
@@ -3,7 +3,9 @@ package com.okta.oidcflows.controller;
 import com.okta.oidcflows.config.TenantConfig;
 import com.okta.oidcflows.service.OIDCService;
 import org.apache.http.auth.AuthenticationException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +18,9 @@ import java.util.stream.Collectors;
 @Controller
 public class FlowTypeController {
 
+    @Value("#{ @environment['okta.zork.url'] }")
+    private String oktaZorkUrl;
+
     private TenantConfig tenantConfig;
     private OIDCService oidcService;
 
@@ -25,7 +30,8 @@ public class FlowTypeController {
     }
 
     @RequestMapping(value = TenantConfig.FLOW_REDIRECT_URI, method = RequestMethod.GET)
-    public String showCode(HttpServletRequest req) {
+    public String showCode(Model model) {
+        model.addAttribute("oktaZorkUrl", oktaZorkUrl);
         return "flow_results";
     }
 
